dd3970c0d7e85d486eafaf03329d685c498af228
AlfoVar <alfonsoingsoftware@gmail.com>
2024-08-14 21:22:16 -0500

    first commit add service_objects CRUD products

---
+++ app/services/Products/create_product.rb
@@ -0,0 +1,17 @@
+module Products
+    class CreateProduct
+        def initialize(params)
+          @params = params
+        end
+        
+        def call
+            product = Product.new(@params)           
+            if product.save
+                { success: true, product: product }
+            else
+                { success: false, error: product.errors.full_message }
+            end
+        end
+    end
+end
+            
---
+++ app/services/Products/update_product.rb
@@ -0,0 +1,22 @@
+module Products
+    class UpdateProduct
+        def initialize(product, params)
+            @product = product
+            @params = params
+        end
+
+        def call
+            return @product unles valid_params?
+
+            @product.update(@params)
+            @product
+        end
+
+        private
+
+        def valid_params?
+            @params[:name].present? && @params[:description].present? && @params[:price].present?
+        end
+    end
+end
+
